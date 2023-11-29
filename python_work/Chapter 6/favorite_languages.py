48aeb18d786355a76907919a7b6b0fe818829fc8
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-11-29 08:56:24 +0200

    fl + mu files

--- python_work/Chapter 6/favorite_languages.py
+++ python_work/Chapter 6/favorite_languages.py
@@ -1,26 +1,29 @@
 favorite_languages = {
-    'jen':'python',
-    'sarah':'c',
-    'edward':'ruby',
-    'phil':'python',
+    'jen':['python','ruby'],
+    'sarah':['c'],
+    'edward':['ruby','go'],
+    'phil':['python', 'haskell'],
 }
 
-language = favorite_languages['sarah'].title()
+language = favorite_languages['sarah']
 
 print(f"Sarah's favorite language is {language}.")
 
-for name,language in favorite_languages.items():
-    print(f"{name.title()}'s favorite language is {language.title()}.")
+for name,languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages is are:")
+    for language in languages:
+        print(f"\t{language.title()}")
 
 print('\n')
 
-friends = ['phil','sarah']
-for name in favorite_languages.keys():
-    print(f"Hi {name.title()}.")
+friends = ['phil', 'sarah']
 
+for name, languages in favorite_languages.items():
+    print(f"Hi {name.title()}.")
     if name in friends:
-        language = favorite_languages[name].title()
+        language = ', '.join(languages).title()
         print(f"\t{name.title()}, I see you love {language}!")
+
 
 if 'erin' not in favorite_languages.keys():
     print("Erin, please take our poll!")
@@ -30,6 +33,6 @@ for name in sorted(favorite_languages.keys()):
 
 print('\n')
 
-print("The folowing languages have been mentioned:")
-for language in set(favorite_languages.values()):
+print("The following languages have been mentioned:")
+for language in set(language for languages in favorite_languages.values() for language in languages):
     print(language.title())
