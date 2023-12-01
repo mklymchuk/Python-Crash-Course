31e24e2d3bfa2ce053b0eda0c46045d667c74b27
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-12-01 16:41:35 +0200

    chapter 7

--- python_work/Chapter 7/parrot.py
+++ python_work/Chapter 7/parrot.py
@@ -1,2 +1,10 @@
-message = input("Tell something, and I will repeat it back to you: ")
-print(message)
+prompt = "\nTell something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+active = True
+while active:
+    message = input(prompt)
+    
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
