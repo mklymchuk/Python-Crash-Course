1ff13aec7842208a6710456cb25dce2733ca42d6
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-12-04 23:07:21 +0200

    chapter 8 8-1 8-2

---
+++ python_work/Chapter 8/favorite_book_8-2.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}")
+    
+favorite_book('harry potter')
+favorite_book('lord of the rings')
---
+++ python_work/Chapter 8/message_8-1.py
@@ -0,0 +1,6 @@
+def display_message(message):
+    print(message)
+    
+chapter_8_message = "I learn in this chapter about the functions"
+
+display_message(chapter_8_message)
