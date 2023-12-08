67291e8661e91cb1da365e4773053e83917afad3
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-12-08 17:58:00 +0200

    9_1 changes, 9-4 add

--- python_work/Chapter 9/number_served_9-4.py
+++ python_work/Chapter 9/number_served_9-4.py
@@ -5,6 +5,7 @@ class Restaurant:
         """Inicialize restaurant name and cuisine type"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
         
     def describe_restaurant(self):
         """Restaurant description"""
@@ -14,6 +15,15 @@ class Restaurant:
         """Message if restaurant is open"""
         print(f"Restaurant {self.restaurant_name} is open")
         
+    def set_number_served(self, numbers):
+        """Set number of served tables"""
+        self.number_served = numbers
+        
+    def increment_number_served(self, numbers):
+        """Increment numbers of served tables"""
+        self.number_served += numbers
+        
+        
 italian_restaurant = Restaurant('Italiano', 'italian')
 
 print(f"Restaurant {italian_restaurant.restaurant_name}")
@@ -21,3 +31,17 @@ print(f"Cuisine {italian_restaurant.cuisine_type}")
 
 italian_restaurant.describe_restaurant()
 italian_restaurant.open_restaurant()
+
+restaurant = Restaurant('New rest', 'new food')
+
+restaurant.number_served = 23
+print(restaurant.number_served)
+
+restaurant.number_served = 12
+print(restaurant.number_served)
+
+restaurant.set_number_served(10)
+print(restaurant.number_served)
+
+restaurant.increment_number_served(10)
+print(restaurant.number_served)
