8a7d79169742b18b432ce54ece28a4be16bba4d2
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-12-29 23:19:26 +0200

    run game, screen, star on the screen

--- python_work/alien_invasion/Chapter_13/starts_13_1.py
+++ python_work/alien_invasion/Chapter_13/starts_13_1.py
@@ -1,4 +1,9 @@
+import sys
+
 import pygame
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
 class GridOfStars:
     """Class to make greed of stars on the screen"""
@@ -7,5 +12,31 @@ class GridOfStars:
         """Initialization of screen"""
         pygame.init()
         
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         
+        pygame.display.set_caption("Greed of stars")
+        
+        self.image = pygame.image.load('python_work/alien_invasion/images/gold_star.bmp')
+        self.rect = self.image.get_rect()
+        
+        self.bg_color = (255, 255, 255)
+        
+    def run_game(self):
+        """Loor where game runs"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+            self._grid_of_stars()
+            self.screen.fill(self.bg_color)
+            self.screen.blit(self.image, self.rect)
+            
+            pygame.display.flip()
+            
+    def _grid_of_stars(self):
+        """Make grid of stars"""
+        
+if __name__ == '__main__':
+    grid_of_stars = GridOfStars()
+    grid_of_stars.run_game()
