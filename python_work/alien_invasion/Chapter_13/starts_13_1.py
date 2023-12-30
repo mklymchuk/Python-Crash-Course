f585b7e2403c910eaef29a0827c3f3a6709378c2
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-12-30 17:17:41 +0200

    attempt to make grid of stars

--- python_work/alien_invasion/Chapter_13/starts_13_1.py
+++ python_work/alien_invasion/Chapter_13/starts_13_1.py
@@ -17,8 +17,7 @@ class GridOfStars:
         pygame.display.set_caption("Greed of stars")
         
         self.image = pygame.image.load('python_work/alien_invasion/images/gold_star.bmp')
-        self.rect = self.image.get_rect()
-        
+        self.star_width, self.star_height = self.image.get_rect().size
         self.bg_color = (255, 255, 255)
         
     def run_game(self):
@@ -30,12 +29,20 @@ class GridOfStars:
                     
             self._grid_of_stars()
             self.screen.fill(self.bg_color)
-            self.screen.blit(self.image, self.rect)
             
             pygame.display.flip()
             
     def _grid_of_stars(self):
         """Make grid of stars"""
+        number_of_stars_in_row = SCREEN_WIDTH // self.star_width
+        number_of_rows = SCREEN_HEIGHT // self.star_height
+        
+        for row_number in range(number_of_rows):
+            for star_number in range(number_of_stars_in_row):
+                star_x = star_number * self.star_width
+                star_y = row_number * self.star_height
+                star_rect = pygame.Rect(star_x, star_y, self.star_width, self.star_height)
+                self.screen.blit(self.image, star_rect.topleft)
         
 if __name__ == '__main__':
     grid_of_stars = GridOfStars()
