36c3d5794869757dc7cd60aef88a5065f2220c9c
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-12-30 17:21:28 +0200

    grid of stars

--- python_work/alien_invasion/Chapter_13/starts_13_1.py
+++ python_work/alien_invasion/Chapter_13/starts_13_1.py
@@ -1,12 +1,11 @@
 import sys
-
 import pygame
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 class GridOfStars:
-    """Class to make greed of stars on the screen"""
+    """Class to make grid of stars on the screen"""
     
     def __init__(self):
         """Initialization of screen"""
@@ -14,30 +13,31 @@ class GridOfStars:
         
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         
-        pygame.display.set_caption("Greed of stars")
+        pygame.display.set_caption("Grid of stars")
         
         self.image = pygame.image.load('python_work/alien_invasion/images/gold_star.bmp')
         self.star_width, self.star_height = self.image.get_rect().size
+        
         self.bg_color = (255, 255, 255)
         
     def run_game(self):
-        """Loor where game runs"""
+        """Loop where game runs"""
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                     
             self._grid_of_stars()
-            self.screen.fill(self.bg_color)
-            
             pygame.display.flip()
             
     def _grid_of_stars(self):
         """Make grid of stars"""
-        number_of_stars_in_row = SCREEN_WIDTH // self.star_width
-        number_of_rows = SCREEN_HEIGHT // self.star_height
+        self.screen.fill(self.bg_color)
         
-        for row_number in range(number_of_rows):
+        number_of_stars_in_row = SCREEN_WIDTH // self.star_width
+        number_rows = SCREEN_HEIGHT // self.star_height
+        
+        for row_number in range(number_rows):
             for star_number in range(number_of_stars_in_row):
                 star_x = star_number * self.star_width
                 star_y = row_number * self.star_height
