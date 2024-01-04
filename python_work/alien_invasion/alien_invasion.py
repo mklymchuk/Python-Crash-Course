86d98601a2c5c0eb69a0e4d99c62a31f07fa5b31
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-01-04 16:08:56 +0200

    check collision between alien and bullet, and remove them if true. Spawn a new alien fleet, when first one destroyed

--- python_work/alien_invasion/alien_invasion.py
+++ python_work/alien_invasion/alien_invasion.py
@@ -83,7 +83,21 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        #print(len(self.bullets))           
+        #print(len(self.bullets))
+        
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet() 
         
     def _update_aliens(self):
         """Check if the fleet is at an edge,
