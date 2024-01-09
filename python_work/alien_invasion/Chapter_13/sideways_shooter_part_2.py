b4591790e2f1c2662ce99946ebb30f058e83e13c
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-01-09 18:34:46 +0200

    refactoring

--- python_work/alien_invasion/Chapter_13/sideways_shooter_part_2.py
+++ python_work/alien_invasion/Chapter_13/sideways_shooter_part_2.py
@@ -94,10 +94,18 @@ class SidewaysShooterPart2:
             if bullet.rect.left >= self.settings.screen_width:
                 self.bullets.remove(bullet)
                 
-        # Check for any bullets that have hit aliens
-        # If so, get rid of the bullet and the alien.
-        collision = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        self._check_bullet_alien_collisions()
                 
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collision = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
+        
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions of all
         aliens in the fleet"""
