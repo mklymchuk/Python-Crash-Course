a669453e33df7713d86f04af7b0acf0d256c4283
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-01-11 17:24:18 +0200

    working on ship movement

--- python_work/alien_invasion/Chapter_14/target_practice_14_2.py
+++ python_work/alien_invasion/Chapter_14/target_practice_14_2.py
@@ -15,16 +15,19 @@ class TargetPractice:
         
         pygame.init()
         
-        self.image = pygame.image.load(SHIP_IMAGE)
-        self.image_rect = self.image.get_rect()
+        self.ship = pygame.image.load(SHIP_IMAGE)
+        self.ship_rect = self.ship.get_rect()
         
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.screen_rect = self.screen.get_rect()
         pygame.display.set_caption("Target practice")
         
-        self.image_rect.midleft = self.screen_rect.midleft
+        self.ship_rect.midleft = self.screen_rect.midleft
         
         self.bg_color = (224, 255, 255)
+        
+        # Movement flag
+        self.moving_up = False
         
     def run_game(self):
         """Starts the main loop for the game"""
@@ -39,11 +42,25 @@ class TargetPractice:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                
+            if event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+    
+    def _check_keydown_events(self, event):
+        """Check if any key pressed"""
+        if event.key == pygame.K_UP:
+            self._update()
+        elif event.key == pygame.K_DOWN:
+            self.ship_rect.y += 1
+            
+    def _update(self):
+        """Update the ship's position based on the movement flag"""
+        if self.moving_up:
+            self.ship_rect.y -= 1
+                            
     def _update_screen(self):
         """Update screen"""
         self.screen.fill(self.bg_color)
-        self.screen.blit(self.image, self.image_rect)
+        self.screen.blit(self.ship, self.ship_rect)
             
 if __name__ == '__main__':
     """Making an instance of Target Practice"""
