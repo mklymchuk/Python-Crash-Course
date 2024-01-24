adb6c4b70e6f71e13641b60eff1d0621319c9686
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-01-24 14:41:19 +0200

    cache

--- python_work/alien_invasion/Chapter_13/sideways_shooter_part_2_settings.py
+++ python_work/alien_invasion/Chapter_13/sideways_shooter_part_2_settings.py
@@ -16,7 +16,7 @@ class SidewaysShooterPart2Settings:
         
         # Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 15
         self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
