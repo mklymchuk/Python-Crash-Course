374b486dddd0c5ce8412a8acea1969b114f99ff8
mklymchuk <mklymchuk1994@gmail.com>
2024-01-24 14:39:18 +0200

    sideways shooter play button

--- python_work/alien_invasion/Chapter_13/sideways_shooter_part_2_settings.py
+++ python_work/alien_invasion/Chapter_13/sideways_shooter_part_2_settings.py
@@ -2,7 +2,7 @@ class SidewaysShooterPart2Settings:
     """A class to store sidewayshooter settings"""
     
     def __init__(self):
-        """Game settings initialization"""
+        """Initialize the game's static settings"""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 720
@@ -11,19 +11,34 @@ class SidewaysShooterPart2Settings:
         self.bg_color = (230, 230, 230)
         
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
         
         # Bullet settings
-        self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 15
         self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
         
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_move_left_speed = 10
+        
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        
+        self.initialize_dynamic_settings()
+        
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+        
         # fleet_direction of -1 represents down; 1 represents up
         self.fleet_direction = -1
+        
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
         
