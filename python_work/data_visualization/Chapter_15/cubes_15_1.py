30fea0433a3795b904145626eeea70aaf25d56c3
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-10 21:14:50 +0200

    ex 15_2

--- python_work/data_visualization/Chapter_15/cubes_15_1.py
+++ python_work/data_visualization/Chapter_15/cubes_15_1.py
@@ -5,6 +5,7 @@ y_values = [x ** 3 for x in x_values]
 
 plt.style.use('_classic_test_patch')
 fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues, s = 10) #type: ignore
 
 ax.plot(x_values, y_values, linewidth = 3)
 
@@ -13,7 +14,7 @@ ax.set_title("Cubic numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
 ax.set_ylabel("Cube of Value", fontsize = 14)
 
-# Set size of tick labels
-ax.tick_params(axis = 'both', labelsize = 14)
+# Set size of tick labels.
+ax.tick_params(axis = 'both', which = 'major', labelsize = 14)
 
 plt.show()
