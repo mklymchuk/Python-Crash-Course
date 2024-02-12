52632a131eee1e5ce219ecffd594d9ea887699fc
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-12 09:59:05 +0200

    random walk visualization

---
+++ python_work/data_visualization/Chapter_15/rw_visual.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    points_numbers = range(rw.num_points)
+    ax.scatter(
+        rw.x_values, rw.y_values, c = points_numbers,
+        cmap = plt.cm.Blues, edgecolors = 'none', s = 15 # type: ignore
+        ) 
+    plt.show()
+    
+    keep_running = input("Make another walk?(y/n):")
+    if keep_running == 'n':
+        break
