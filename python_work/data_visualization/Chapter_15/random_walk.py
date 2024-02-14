1fb36ca77944d5fd784e5e22d087f51ab94e03cc
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-14 15:32:03 +0200

    die rolls vizualization, random walk refactoring

--- python_work/data_visualization/Chapter_15/random_walk.py
+++ python_work/data_visualization/Chapter_15/random_walk.py
@@ -3,7 +3,7 @@ from random import choice
 class RandomWalk:
     """A class to generate random walks."""
     
-    def __init__(self, num_points = 5000):
+    def __init__(self, num_points=5000):
         """Initialize attributes of a walk."""
         self.num_points = num_points
         # All walks start at (0, 0)
@@ -12,23 +12,10 @@ class RandomWalk:
         
     def fill_walk(self):
         """Calculate all the points in the walk."""
-        self.x_step = self.get_step()
-        self.y_step = self.get_step()
         
-    def get_step(self):
-        """Determination of direction and distance for each step"""
-            
-        # Keep taking steps until the walk reaches the desired lenght.
         while len(self.x_values) < self.num_points:
-            
-            # Decide which direction to go and how far to go in that direction.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-            
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
             
             # Reject moves that go nowhere.
             if x_step == 0 and y_step == 0:
@@ -40,3 +27,9 @@ class RandomWalk:
             
             self.x_values.append(x)
             self.y_values.append(y)
+            
+    def get_step(self):
+        """Determination of direction and distance for each step"""
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        return direction * distance
