967d2937ea9a8df1198e2bec49f1ed4fbf50f15e
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-15 21:26:53 +0200

    Visualazing D6 and D10 dices. Visualization of D8 and D8.

--- python_work/data_visualization/Chapter_15/two_D8s_15_6.py
+++ python_work/data_visualization/Chapter_15/two_D8s_15_6.py
@@ -3,13 +3,13 @@ from plotly import offline
 
 from die import Die
 
-# Create a D6 and a D10.
-die_1 = Die()
-die_2 = Die(10)
+# Create two D8 die.
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store result in a list
 results = []
-for roll_num in range(50_000):
+for roll_num in range(5_000_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
     
@@ -26,6 +26,6 @@ data = [Bar(x = x_values, y = frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title = 'Result of rolling two D6 and D10 50000 times',
+my_layout = Layout(title = 'Result of rolling two D8 die 5000000 times',
                    xaxis = x_axis_config, yaxis = y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename = 'd6_d10.html')
+offline.plot({'data': data, 'layout': my_layout}, filename = 'd8_d8.html')
