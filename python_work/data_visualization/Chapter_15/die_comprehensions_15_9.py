10acc3f26d29c83f39bc38f3f93c98142787a6e4
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-16 13:24:16 +0200

    ex 15_9

--- python_work/data_visualization/Chapter_15/die_comprehensions_15_9.py
+++ python_work/data_visualization/Chapter_15/die_comprehensions_15_9.py
@@ -8,17 +8,11 @@ die_1 = Die()
 die_2 = Die()
 
 # Make some rolls, and store result in a list
-results = []
-for roll_num in range(5_000_000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for _ in range(5_000_000)]
     
 # Analyze the results.
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(2, max_result + 1)]
     
 # Visualize the results.
 x_values = list(range(2, max_result + 1))
