587d2bb9505308a59964e3853a83c70d1dfdac3c
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-18 11:19:35 +0200

    two dies rolling in ex 15_10

--- python_work/data_visualization/Chapter_15/practicing_with_both_libraries_15_10.py
+++ python_work/data_visualization/Chapter_15/practicing_with_both_libraries_15_10.py
@@ -1,26 +1,34 @@
 import matplotlib.pyplot as plt
+import plotly.express as px
 from die import Die
 
 # Create a die
 die_1 = Die()
+die_2 = Die()
 
 # Make some rolls
 results = []
 for roll_num in range(1000):
-    result = die_1.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
     
-# Analyze the rezults.
+# Analyze the results.
 frequencies = []
-for value in range (1, die_1.num_sides + 1):
+die_max_value = die_1.num_sides + die_2.num_sides
+for value in range(2, die_max_value + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
     
+# Plot the frequencies
 fig, ax = plt.subplots()
-ax.plot(results, linewidth = 3)
+ax.bar(
+    range(2, die_max_value + 1), frequencies, edgecolor='white',
+    linewidth=0.7
+    )
 
 # Set chart title and label axes.
-ax.set_title("Die 1000 rolls", fontsize = 24)
-ax.set_xlabel("Roll number",  fontsize = 14)
-ax.set_ylabel("Die sides",  fontsize = 14)
+ax.set_title("Die 1000 rolls", fontsize=24)
+ax.set_xlabel("Die sides", fontsize=14)
+ax.set_ylabel("Frequency", fontsize=14)
+
 plt.show()
