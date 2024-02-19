3ddd6969d570ad949e5ed51d8787453951f7580a
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-19 16:52:08 +0200

    ex 15_10

--- python_work/data_visualization/Chapter_15/practicing_with_both_libraries_15_10.py
+++ python_work/data_visualization/Chapter_15/practicing_with_both_libraries_15_10.py
@@ -37,6 +37,32 @@ plt.show()
 
 """Random walk"""
 
-rw = RandomWalk(50_000)
-rw.fill_walk()
+while True:
+    # Make a random walk
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
+    # Create a trace for scatter plot
+    trace = go.Scatter(x = rw.x_values,
+                    y = rw.y_values,
+                    mode = 'markers', 
+                    marker = dict( size = 4, color = list(range(rw.num_points)), 
+                                    colorscale = 'Blues', opacity = 0.8 ))
+    
+        # Create a layout
+    layout = go.Layout(
+        title='Random Walk',
+        xaxis=dict(title='X'),
+        yaxis=dict(title='Y'),
+        showlegend=False
+    )
+                    
+    # Create figure object
+    fig = go.Figure(data=[trace], layout=layout)
+
+    # Show the plot
+    fig.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running.lower() == 'n':
+        break
