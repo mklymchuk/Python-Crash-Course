000440d3c4201e8ccdaaa38db59a815474c37070
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-02-25 09:27:38 +0200

    Earthqukes visualization.

--- python_work/data_visualization/Chapter_16/eq_explore_data.py
+++ python_work/data_visualization/Chapter_16/eq_explore_data.py
@@ -1,5 +1,8 @@
 import json
 
+from plotly.graph_objs import Scattergeo, Layout 
+from plotly import offline
+
 # Explore the structure of the data.
 filename = 'python_work/data_visualization/Chapter_16/data/eq_data_1_day_m1.json'
 with open(filename) as f:
@@ -7,4 +10,26 @@ with open(filename) as f:
     readable_file = 'python_work/data_visualization/Chapter_16/data/readable_eq_data.json'
     with open(readable_file, 'w') as f:
         json.dump(all_eq_data, f, indent=4)
+    
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    
+# Map the earthquakes.
+data = [{
+    'type':'scattergeo', 
+    'lon':lons, 
+    'lat':lats
+    }]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
     
