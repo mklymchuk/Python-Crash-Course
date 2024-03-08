5c9cc877edd8b8ede8c329665c55359d4833f1b2
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-03-08 10:10:29 +0200

    Adding models Pizza and Topping.

--- python_work/web_applications/Chapter_18/pizzeria/pizzas/models.py
+++ python_work/web_applications/Chapter_18/pizzeria/pizzas/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
-# Create your models here.
 class Pizza(models.Model):
     """A pizza to create."""
-    text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
+
+class Topping(models.Model):
+    """Pizza toppings."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+    
+    def __str__(self):
+        """Returning a string representation of the model."""
+        return f"{self.name[:50]}"
