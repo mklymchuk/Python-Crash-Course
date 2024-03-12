c9696904d7841e8aaab738fb0a8be587b976a02b
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-03-12 14:53:55 +0200

    Ex 18_5.

--- python_work/web_applications/Chapter_18/meal_planner/meal_plans/apps.py
+++ python_work/web_applications/Chapter_18/meal_planner/meal_plans/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class MealPlansConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meal_plans'
--- python_work/web_applications/Chapter_18/meal_planner/meal_plans/models.py
+++ python_work/web_applications/Chapter_18/meal_planner/meal_plans/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Meal(models.Model):
+    """A meal description."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text
+
+class MealPlan(models.Model):
+    """Something specific about a meal."""
+    meal_description = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Meal plans'
+   
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"Meal plan: {self.text[:50]}"
