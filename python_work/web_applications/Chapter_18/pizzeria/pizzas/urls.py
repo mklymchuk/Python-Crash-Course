5d7654915962a9f69624e14687cd256301667ccd
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-03-14 09:47:27 +0200

    Refactor templates and URLs for pizzas app.

--- python_work/web_applications/Chapter_18/pizzeria/pizzas/urls.py
+++ python_work/web_applications/Chapter_18/pizzeria/pizzas/urls.py
@@ -6,6 +6,13 @@ from . import views
 
 app_name = 'pizzas'
 urlpatterns = [
-    # Home page
+    
+    # Home page.
     path('', views.index, name='index'),  # Corrected 'name' spelling
+    
+    # Page that shows all pizzas.
+    path('pizzas/', views.pizzas, name='pizzas'),
+    
+    # Detail page for a single pizza.
+    path('pizzas/<int:pizza_id>/', views.pizzas, name='pizza'),
 ]
