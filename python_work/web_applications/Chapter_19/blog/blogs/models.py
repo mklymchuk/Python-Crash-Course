4482244604c750907c5846e24224f314adfda2ff
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-03-18 10:03:34 +0200

    Creating an app and model for ex 19-1.

---
+++ python_work/web_applications/Chapter_19/blog/blogs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class BlogPost(models.Model):
+    """A post of blog."""
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title  
+
