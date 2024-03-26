9aee04a87221dfda35955cae31bd02fb74e09b32
mklymchuk <m.klymchuk@student.po.edu.pl>
2024-03-26 12:03:06 +0200

    Ex 19-5.

--- python_work/web_applications/Chapter_19/blog/blogs/models.py
+++ python_work/web_applications/Chapter_19/blog/blogs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
     """A post of blog."""
     title = models.CharField(max_length=100)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         """Return a string representation of the model."""
--- python_work/web_applications/Chapter_19/blog/blogs/views.py
+++ python_work/web_applications/Chapter_19/blog/blogs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import BlogPost
 from .forms import BlogForm
@@ -7,15 +9,18 @@ def index(request):
     """The home page for Blogs."""
     return render(request, 'blogs/index.html')
 
+@login_required
 def blogs(request):
     """Show all blogs."""
-    blogs = BlogPost.objects.order_by('date_added')
+    blogs = BlogPost.objects.filter(owner=request.user).order_by('date_added')
     context = {'blogs': blogs}
     return render(request, 'blogs/blogs.html', context)
 
+@login_required
 def blog(request, blog_id):
     """Show a single blog."""
     blog = BlogPost.objects.get(id=blog_id)
+    check_blog_owner(request, blog)    
     context = {
         'blog': blog,
         'blog_title': blog.title,
@@ -24,22 +29,26 @@ def blog(request, blog_id):
         }
     return render(request, 'blogs/blog.html', context)
 
+@login_required
 def new_blog(request):
     if request.method == 'POST':
         form = BlogForm(request.POST)
         if form.is_valid():
-            blog = form.save()  # Save the form and get the saved blog object
+            new_blog = form.save(commit=False)
+            new_blog.owner = request.user
+            new_blog.save()  # Save the form and get the saved blog object
             # Redirect to the 'blog' URL with the blog's ID as an argument
-            return redirect('blogs:blog', blog_id=blog.id)
+            return redirect('blogs:blog', blog_id=new_blog.id)
     else:
         form = BlogForm()
     context = {'form': form}
     return render(request, 'blogs/new_blog.html', context)
 
+@login_required
 def edit_blog(request, blog_id):
     """Edit an existing blog."""
     blog = BlogPost.objects.get(id=blog_id)
-    
+    check_blog_owner(request, blog)    
     if request.method != 'POST':
         # Initial request; pre-fill form with the current blog.
         form = BlogForm(instance=blog)
@@ -51,3 +60,9 @@ def edit_blog(request, blog_id):
             return redirect('blogs:blog', blog_id=blog.id)
     context = {'blog': blog, 'form': form}
     return render(request, 'blogs/blog_edit.html', context)
+
+def check_blog_owner(request, blog):
+    """Check actual blog owner."""
+    # Make sure the blog belongs to the current user.
+    if blog.owner != request.user:
+        raise Http404
